5e15cec80e466f6186a7b4ea25724b0f796c7ecb
Ankit Jindal <jindalankit940@gmail.com>
2025-07-12 20:01:24 +0530

    Added code

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,16 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'  // Make sure to import this
+import jest from 'eslint-plugin-jest'    // Make sure to import jest here
+
+// const js = require('@eslint/js');
+// const globals = require('globals');
+// const reactHooks = require('eslint-plugin-react-hooks');
+// const reactRefresh = require('eslint-plugin-react-refresh');
+// const { defineConfig, globalIgnores } = require('eslint/config');
+// const react = require('eslint-plugin-react');
+// const jest = require('eslint-plugin-jest');
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,15 +22,27 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      // 'plugin:jest/recommended',
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        test: 'readonly',    // Add jest globals here
+        expect: 'readonly',  // Add jest globals here
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      // 'react-hooks': reactHooks,
+      // 'react-refresh': reactRefresh,
+      jest,
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
